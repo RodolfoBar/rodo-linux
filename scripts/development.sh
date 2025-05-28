b9d69f6c9079c690beb6af56fe2aa64a51be619e
Rodolfo Barcelli Jo <rodolfo.barcelli@gmail.com>
2025-05-29 01:26:26 +0800

    add development script

---
+++ scripts/development.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "
+
+
+
+------------------
+DEVELOPEMENT SETUP
+------------------
+
+
+
+"
+
+# {{{ Packages
+PACKAGES_DIR="$HOME/Repos/rodo-linux/package-lists"
+
+for line in $(cat $PACKAGES_DIR/development.txt); do
+    sudo pacman -S --needed --noconfirm $line
+done
+# }}}
+
+# {{{ NVM
+if [[ -z $(cat $HOME/.bashrc | grep nvm) ]]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+fi
+# }}}
