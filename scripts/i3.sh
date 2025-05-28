7de1795990ab027a3cecd4e6c61dddc523b54d11
Rodolfo Barcelli Jo <rodolfo.barcelli@gmail.com>
2025-05-29 02:12:49 +0800

    Update i3 script with a commented out section for nvidia drivers so I don't forget

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -16,6 +16,14 @@ i3 Desktop Environment
 
 PACKAGES_DIR="$HOME/Repos/rodo-linux/package-lists"
 
+# {{{ Graphics Drivers
+# if [[ ! -z $(lspci | grep VGA | grep NVIDIA) ]]; then
+# 	sudo pacman -S --noconfirm --needed nvidia-open-dkms nvidia-settings
+# elif [[ ! -z $(lspci | grep VGA) ]]; then
+# 	sudo pacman -S --noconfirm --needed 
+# fi
+# }}}
+
 # {{{ i3 packages
 for line in $(cat $PACKAGES_DIR/i3.txt); do
 	sudo pacman -S --noconfirm --needed $line
