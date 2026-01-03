1bcb32d0cb75086ecef7734030d2c599be6d3c8f
Rodolfo Barcelli Jo <rodolfo.barcelli@gmail.com>
2026-01-03 17:30:26 +0800

    Update: Disable steam installer to prevent nvidia stuff being erroneously installed

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -15,11 +15,13 @@ GAMING SETUP
 "
 
 # {{{ Graphics Drivers
-if [[ ! -z $(lspci | grep VGA | grep nvidia) ]]; then
-    sudo pacman -S --noconfirm --needed nvidia-utils lib32-nvidia-utils
-fi
+# if [[ ! -z $(lspci | grep VGA | grep nvidia) ]]; then
+#     sudo pacman -S --noconfirm --needed nvidia-utils lib32-nvidia-utils
+# fi
 # }}}
 
-sudo pacman -S --noconfirm --needed ttf-liberation steam
+# sudo pacman -S --noconfirm --needed ttf-liberation steam
+
+echo "This is not working"
 
 sleep 1
